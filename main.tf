4c90b9f66f2e141ef01202566704230f2a31cc16
Dinakara Ambepitiya <dinakaranadun@gmail.com>
2025-05-25 19:58:54 +0530

    updates in main.tf and outputs.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 terraform {
+  backend "s3" {
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,11 +17,56 @@ provider "aws" {
   region = var.region
 }
 
+# Get current AWS Account ID
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id = data.aws_caller_identity.current.account_id
+}
+
+# EC2 Instance
 resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
   tags = {
     Name = var.instance_name
+  }
+}
+
+# S3 Bucket 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "${local.account_id}-terraform-states"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+# DynamoDB table  locking
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Terraform Lock Table"
   }
 }
